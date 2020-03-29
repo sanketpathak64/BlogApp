d7dd437747abac441352e8fb3c5e055b795aa93d
sanket pathak <sanketpathak64@gmail.com>
2020-03-29 17:35:53 +0530

    connected to db and added basic blog

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require('express'),
+app = express(),
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/blog_app",{ useUnifiedTopology: true,useNewUrlParser: true});
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type:Date,default:Date.now}
+})
+var Blog = mongoose.model("Blog",blogSchema);
+// Blog.create({
+//     title:"Test Blog",
+//     image:"https://images.unsplash.com/photo-1445985543470-41fba5c3144a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+//     body:"this is my blog"
+// });
+//restful routes 
+
+app.get("/",function(req,res){
+    res.redirect("/blogs");
+});
+
+
+app.get("/blogs",function(req,res){
+    
+    Blog.find({},function(err,blogs){
+        if(err){
+            console.log(err);
+        }
+        else {
+            res.render("index",{blogs:blogs});
+        }
+    })
+    
+    //
+});
+
+
+
+
+app.listen(3000,function(){
+    console.log("server is running");
+})
